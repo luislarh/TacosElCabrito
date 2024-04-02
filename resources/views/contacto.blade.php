7dce4f00fcb58e3a6bbc00a44c3fb03ce6e01ee9
luislarh <75351293+luislarh@users.noreply.github.com>
2024-04-01 23:19:19 -0600

    Update contacto.blade.php

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -8,7 +8,7 @@
     <!-- Fontawesome Link -->
     <link href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" rel="stylesheet">
     <!-- Tu archivo CSS personalizado -->
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     <link rel="icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
